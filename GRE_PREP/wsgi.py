3558bdfa3f97288a43f6b2e084fef580b223a62f
Saroj Raj Sharma <014saroz@gmail.com>
2020-09-20 12:25:08 +0545

    Rename project

--- GRE_PREP/wsgi.py
+++ GRE_PREP/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for GRE_QUIZ project.
+WSGI config for GRE_PREP project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GRE_QUIZ.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GRE_PREP.settings")
 
 application = get_wsgi_application()
